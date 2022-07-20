bcb95202befad2944275ed5d8e787701500fe4c1
electrocodeur <77399487+electrocodeur@users.noreply.github.com>
2022-07-20 21:40:16 +0200

    Add files via upload

---
+++ DHT11_stm32.ino
@@ -0,0 +1,23 @@
+#include <DHT.h>;
+
+#define DHT11_PIN PA0
+#define DHTTYPE DHT11
+
+DHT dht(DHT11_PIN, DHTTYPE);
+
+float temp, hum;
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+}
+
+void loop() {
+  temp = dht.readTemperature();
+  hum = dht.readHumidity();
+  Serial.print(temp);
+  Serial.println(" °C");
+  Serial.print(hum);
+  Serial.println(" %");
+  delay(1000);
+}
